1b7be2956cd044cf7289498eae1112ed7f9dddee
h662Olbm <h662@olbm.app>
2023-04-05 22:27:02 +0900

    shuffle array

--- src/pages/worldcup.jsx
+++ src/pages/worldcup.jsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import animalData from "../animalData.json";
+
 const Worldcup = () => {
-  return <div>Worldcup</div>;
+  const [shuffleAnimal, setShuffleAnimal] = useState();
+
+  useEffect(() => {
+    let shuffledAnimalData = animalData.sort(() => {
+      return Math.random() - 0.5;
+    });
+
+    setShuffleAnimal(shuffledAnimalData);
+  }, []);
+  useEffect(() => console.log(shuffleAnimal), [shuffleAnimal]);
+
+  return <div className="bg-pink-200 min-h-screen">Worldcup</div>;
 };
 
 export default Worldcup;
